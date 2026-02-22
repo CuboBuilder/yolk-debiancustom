9e4343c69c0e3fbeb9b4f9059e754d5315c9b98d
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2026-02-22 12:32:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ cd /home/container
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
-echo -e "\033[1;33mAttempting to start server with command:\033[0m"
+echo -e "\033[1;33mStarting with:\033[0m"
 echo -e "\033[1;32m${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
