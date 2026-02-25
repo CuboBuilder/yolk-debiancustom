b00ab81c2ba9987006bd86bfdb8e8371fbb34533
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2026-02-25 09:50:02 +0200

    update startup message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@ cd /home/container
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
-echo -e "\033[1;33mStarting Nebula Yolk...\033[0m"
-echo -e "\033[1;32mRunning: ${MODIFIED_STARTUP}\033[0m"
+echo -e "\033[1;35mStarting Nebula Yolk by CuboBuilder...\033[0m"
+echo -e "\033[1;32mRepository: github.com/CuboBuilder/nebula-yolk\033[0m"
+echo -e "\033[1;32mFeel free to add needed packages by using pull requests.\033[0m"
+echo -e "\033[1;36mRunning: ${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
 
 if [ $? -ne 0 ]; then
-    echo -e "\033[1;31mStartup command failed.\033[0m"
+    echo -e "\033[1;31mStartup command failure.\033[0m"
     exit 1
 fi
