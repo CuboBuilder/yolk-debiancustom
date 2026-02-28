528e3f043c891b018e59aec39680d20d373eab62
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2026-02-28 16:09:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,39 +8,9 @@ echo -e "\033[1;32mRepository: github.com/CuboBuilder/nebula-yolk\033[0m"
 echo -e "\033[1;32mFeel free to add needed packages by using pull requests.\033[0m"
 echo -e "\033[1;36mRunning: ${MODIFIED_STARTUP}\033[0m"
 
-set -m
-eval ${MODIFIED_STARTUP} &
-PID=$!
-PGID=$(ps -o pgid= -p $PID | grep -o '[0-9]*')
+eval ${MODIFIED_STARTUP}
 
-SHUTTING_DOWN=0
-
-shutdown() {
-    if [ $SHUTTING_DOWN -eq 1 ]; then return; fi
-    SHUTTING_DOWN=1
-    
-    echo -e "\n\033[1;33mShutdown requested. Stopping process group $PGID...\033[0m"
-    
-    kill -15 -$PGID 2>/dev/null
-    
-    for i in {1..30}; do
-        if ! ps -o pgid= -p $PGID >/dev/null 2>&1; then
-            exit 0
-        fi
-        sleep 1
-    done
-
-    kill -9 -$PGID 2>/dev/null
-    exit 0
-}
-
-trap 'shutdown' SIGTERM SIGINT
-
-while true; do
-    if read -t 5 input; then
-        if [ "$input" = "stop" ]; then shutdown; fi
-    fi
-    if ! ps -o pgid= -p $PGID >/dev/null 2>&1; then break; fi
-done
-
-exit 0
+if [ $? -ne 0 ]; then
+    echo -e "\033[1;31mStartup command failure.\033[0m"
+    exit 1
+fi
